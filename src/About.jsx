0bb102ea9a56c22ab3918ea36454d4f908c03a80
Nimisha-trivedi <nimishatrivedi28@gmail.com>
2021-04-07 01:48:08 +0530

    my first commit

---
+++ src/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import web from "../src/images/img1.jpg";
+import Common from './Common';
+
+
+const About = () => {
+    return (
+        <>
+            <Common name='Welcome to about page' 
+            imgsrc={web} 
+            visit="/contact" 
+            btname="Contact now" />
+        </>
+    );
+};
+export default About;
---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import web from "../src/images/img1.jpg";
+import Common from './Common';
+
+
+const Home = () => {
+    return (
+        <>
+            <Common name="Grow Your bussiness with"
+                imgsrc={web}
+                visit="/service"
+                btname="Get Started" />
+        </>
+    );
+};
+export default Home;
